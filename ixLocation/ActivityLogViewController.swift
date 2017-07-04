0a41e611d4184d9428647136694321689bbbfa1c
Kavilan Nair <kavilann@gmail.com>
2017-07-04 09:49:45 +0200

    Activity Log viewcontroller and settings viewcontroller added

---
+++ ixLocation/ActivityLogViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ActivityLogViewController.swift
+//  ixLocation
+//
+//  Created by Kavilan Nair on 2017/07/04.
+//  Copyright © 2017 Kavilan Nair. All rights reserved.
+//
+
+import UIKit
+
+class ActivityLogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
